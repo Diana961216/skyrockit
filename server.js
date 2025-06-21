5e63ec446f0508ee3e5b5414c9c31ecc79c022e7
DianaArman1216 <dianmap96@icloud.com>
2025-06-20 23:57:47 -0400

    only missing edit and update

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
 const authController = require("./controllers/auth.js");
+const applicationsController = require("./controllers/applications.js");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -30,31 +33,33 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
 }));
+app.use(passUserToView); // Middleware to pass user data to views
 
-const isLoggedIn = (req, res, next) => {
-  if (req.session.user) {
-    next(); // User is logged in, proceed to the next middleware or route handler
-  } else {
-    res.redirect("/auth/sign-in"); // User is not logged in, redirect to sign-in page
-  }
-}
+
+// const isLoggedIn = (req, res, next) => {
+//   if (req.session.user) {
+//     next(); // User is logged in, proceed to the next middleware or route handler
+//   } else {
+//     res.redirect("/auth/sign-in"); // User is not logged in, redirect to sign-in page
+//   }
+// }
 
 // GET /
-app.get("/", (req, res) => {
-    res.render("index.ejs", {
-      user: req.session.user,
-    });
-  });
-
-  app.get("/vip-lounge", (req, res) => {
+  app.get('/', (req, res) => {
+    // Check if the user is signed in
     if (req.session.user) {
-      res.send(`Welcome to the party ${req.session.user.username}.`);
+      // Redirect signed-in users to their applications index
+      res.redirect(`/users/${req.session.user._id}/applications`);
     } else {
-      res.send("Sorry, no guests allowed.");
+      // Show the homepage for users who are not signed in
+      res.render('index.ejs');
     }
   });
 
+
 app.use("/auth", authController);
+app.use(isSignedIn); // Apply isSignedIn middleware to all routes below this point
+app.use('/users/:userId/applications', applicationsController);
   
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
